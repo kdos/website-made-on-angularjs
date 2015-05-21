c33595be60754e74ad57c7f0718646541cc2b480
deeper.komal@gmail.com <kdos>
2015-05-21 22:22:56 +0530

    corrected most front-end mistakes

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.view2', ['ngRoute'])
+angular.module('myApp.view2', ['ngRoute', 'infinite-scroll'])
 
 .config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/view2', {
@@ -10,22 +10,47 @@ angular.module('myApp.view2', ['ngRoute'])
 }])
 
 .controller('View2Ctrl', ['$scope', '$http', function($scope, $http) {
-    $http({
-            method: 'GET',
-            url: 'http://104.236.150.55/api/activity/list/?format=json'
-        })
-        .success(function(response) {
-            $scope.names = response.results;
-            //$scope.name = response.count;
-        })
-        .error(function(data, status) {
-            //$scope.name = status;
+        $scope.isLoading = true; //condition for preloader
+        $http({
+                method: 'GET',
+                url: 'http://104.236.150.55/api/activity/list/?format=json'
+            })
+            .success(function(response) {
+                $scope.names = response.results;
+                $scope.isLoading = false;
+            })
+            .error(function(data, status) {
+                $scope.names = status;
+                $scope.isLoading = true;
+            });
+        $scope.loadMore = function() {
+                var last = $scope.names[$scope.names.length - 1];
+                for (var i = 1; i <= 8; i++) {
+                    $scope.names.push(last + i);
+                }
+            }
+            /*
+            $scope.currentPage = 0;
+            $scope.pageSize = 3;
+            $scope.numberOfPages = function(){
+                return Math.ceil($scope.names.length/$scope.pageSize);
+            }*/
+            /* settings for 'materialize' objects*/
+        $(".button-collapse").sideNav({
+            menuWidth: 400, // Default is 240
+            edge: 'right', // Choose the horizontal origin
+            closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
         });
-    $(".button-collapse").sideNav({
-        menuWidth: 300, // Default is 240
-        edge: 'left', // Choose the horizontal origin
-        closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    });
-    $('.materialboxed').materialbox();
-    $('ul.tabs').tabs()
-}]);
+        $('.materialboxed').materialbox();
+        $('ul.tabs').tabs();
+
+    }])
+    /*
+    .filter('startFrom',[function(){
+        return function(input, start) {
+            start = +start; //parse to int
+            return input.slice(start);
+        }
+    }])
+    */
+;
